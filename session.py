9a7ccf204e0a4116e3acaea8899af6a8e0c03d43
Leonel Quinteros <leo@SDF1>
2012-12-26 14:38:47 -0300

    Delete method fix.

--- session.py
+++ session.py
@@ -19,11 +19,11 @@ SESSION_NAME = 'MemSession'
 # Make the HKEY unique
 HKEY = 'FF&KEKLDkjd-pfjGruUYn%GfsbTRuTEt5454T$315va'
 
-# Global session identifier. 
+# Global session identifier.
 SESSION_ID = 'MemSession'
 
 # Session's cookie duration in seconds.
-MAX_AGE = 3600 
+MAX_AGE = 3600
 
 ##############################
 #                            #
@@ -36,11 +36,11 @@ class MemSession(object):
     _hkey = ''
     _id = ''
     _maxAge = 3600
-    
-    
+
+
     def __init__(self):
         global SESSION_NAME, HKEY, SESSION_ID, MAX_AGE
-        
+
         self._name = SESSION_NAME
         self._hkey = HKEY
         self._id = SESSION_ID
@@ -50,20 +50,20 @@ class MemSession(object):
     def process_request(self, request):
         self.start(request)
         request.memSession = self
-        
+
         return None
 
-    
+
     def process_response(self, request, response):
         self.start(request)
-        
+
         # Renew cookie's max_age
         response.set_cookie(self._name, self._id, self._maxAge)
         response.set_cookie(self._name + '_v', self.generateSessionValidator(self._id), self._maxAge)
-        
+
         return response
-    
-    
+
+
     def start(self, request):
         if not self._name in request.COOKIES:
             self._id = self.generateSessionId(request)
@@ -72,78 +72,79 @@ class MemSession(object):
                 self._id = self.generateSessionId(request)
             else:
                 self._id = request.COOKIES[self._name]
-        
+
         return self._id
-    
-    
+
+
     def generateSessionId(self, request):
         h = hashlib.sha512()
-        
+
         if 'HTTP_USER_AGENT' in request.META:
             h.update(request.META['HTTP_USER_AGENT'])
-        
+
         h.update(self._hkey)
-        
+
         if 'REMOTE_ADDR' in request.META:
             h.update(request.META['REMOTE_ADDR'])
-        
+
         h.update(str(time.time()))
-        
+
         if 'REMOTE_HOST' in request.META:
             h.update(request.META['REMOTE_HOST'])
-        
+
         return h.hexdigest()
-    
-    
+
+
     def generateSessionValidator(self, sessId):
         h = hashlib.sha512()
         h.update(sessId)
         h.update(self._name)
         h.update(self._hkey)
-        
+
         return h.hexdigest()
-    
-    
+
+
     def sessionIsValid(self, sessId, sessV):
         h = hashlib.sha512()
         h.update(sessId)
         h.update(self._name)
         h.update(self._hkey)
-        
+
         if h.hexdigest() == sessV:
             return True
         else:
             return False
-    
-    
+
+
     def read(self, key):
         s = memcache.get(self._name + '.' + self._id)
         if not s:
             s = {}
-        
+
         if key not in s.keys():
             return None
-        
+
         return s[key]
-    
-    
+
+
     def write(self, key, value):
         s = memcache.get(self._name + '.' + self._id)
         if not s:
             s = {}
-        
+
         s[key] = value
         memcache.set(self._name + '.' + self._id, s, (self._maxAge * 20))
-        
+
         return True
-    
+
     def delete(self, key):
         s = memcache.get(self._name + '.' + self._id)
         if not s:
             s = {}
-        
+
         if key in s.keys():
             del s[key]
-        
-        return True
 
+        memcache.set(self._name + '.' + self._id, s, (self._maxAge * 20))
+
+        return True
